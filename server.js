9cdb34ac7fe79f7627a7f67cfd662392b0e06064
malek456 <bouzidimalek01@gmail.com>
2024-07-17 11:00:44 +0100

    add homepage

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
-import express from 'express';
-import connectDB from './config/db.js';  
-import userRoutes from './routes/users.js'; 
-import categoryRoutes from './routes/categories.js';
-import courseRoutes from './routes/courses.js';
-import authRoutes from './routes/auth.user.js'; 
-import participationRoutes from './routes/participation.js';
-
-
-
+import express from "express";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/users.js";
+import categoryRoutes from "./routes/categories.js";
+import courseRoutes from "./routes/courses.js";
+import authRoutes from "./routes/auth.user.js";
+import participationRoutes from "./routes/participation.js";
+import { json } from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-connectDB(); 
+connectDB();
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/categories', categoryRoutes);
-app.use('/api/courses', courseRoutes);
-app.use('/api/auth',authRoutes);
-app.use('/api/participation', participationRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/participation", participationRoutes);
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.use("/", async (req, res) => {
+  res.json({ mesage: "Server started" });
 });
 
-
-
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
